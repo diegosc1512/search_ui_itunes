36ea6afe3d37e69cbf9846126e2959c6dc984cfc
unknown <diegosc1512@gmail.com>
2021-09-28 11:04:01 -0400

    creating services to consume itunes api

--- itunes-search/src/app/shared/itunes.service.ts
+++ itunes-search/src/app/shared/itunes.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Jsonp } from '@angular/http';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/toPromise';
 
-@Injectable({
-  providedIn: 'root'
-})
+const API = {
+  SEARCH: 'https://itunes.apple.com/search?',
+  LOOKUP: 'https://itunes.apple.com/lookup?'
+}
+
+@Injectable()
 export class ItunesService {
 
-  constructor() { }
+  constructor(private jsonp: Jsonp) {
+  }
+
+  public search(searchTerm): Promise {
+    return this.jsonp.get(`${API.SEARCH}callback=JSONP_CALLBACK&media=music&country=US&entity=musicArtist&term=${searchTerm}`)
+      .toPromise()
+      .then(data => data.json().results)
+      .catch(this.handleError)
+  }
+
+  private handleError(error: any): Promise {
+    console.log(error);
+    return Promise.reject(error.message || error);
+  }
 }
