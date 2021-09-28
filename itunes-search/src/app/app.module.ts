2d989af0ae0881b67b697afe07638a8b25006f4d
unknown <diegosc1512@gmail.com>
2021-09-28 14:59:13 -0400

    added component artist and album

--- itunes-search/src/app/app.module.ts
+++ itunes-search/src/app/app.module.ts
@@ -2,19 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+// Include the JSONP module for JSONP support
 import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { ArtistComponent } from './artist/artist.component';
+import { AlbumComponent } from './album/album.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
 
+    // include the JSONP module so it can be used in the application
     JsonpModule
   ],
   providers: [],
--- itunes-search/src/app/shared/itunes.service.ts
+++ itunes-search/src/app/shared/itunes.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Jsonp } from '@angular/http';
 import 'rxjs/add/operator/catch';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import 'rxjs/add/operator/toPromise';
 
 const API = {
@@ -10,18 +12,40 @@ const API = {
 
 @Injectable()
 export class ItunesService {
+  searchTerm = '';
 
+  private _albums = [];
+  private _artistId = 0;
   constructor(private jsonp: Jsonp) {
   }
 
-  public search(searchTerm): Promise {
+  public search(searchTerm: string): Promise<any>  {
     return this.jsonp.get(`${API.SEARCH}callback=JSONP_CALLBACK&media=music&country=US&entity=musicArtist&term=${searchTerm}`)
       .toPromise()
       .then(data => data.json().results)
       .catch(this.handleError)
   }
+  public getAlbums(artistId: number): Promise<any> {
+    if (artistId === this._artistId) {
+      return new Promise(resolve => resolve(this._albums));
+    }
 
-  private handleError(error: any): Promise {
+    this._artistId = artistId;
+
+    return this.jsonp
+      .get(`${API.LOOKUP}callback=JSONP_CALLBACK&entity=album&id=${artistId}`)
+      .toPromise()
+      .then(data => {
+        this._albums = data.json().results.filter((results: any) => {
+          return results.wrapperType === 'collection';
+        });
+
+        return this._albums;
+      })
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
     console.log(error);
     return Promise.reject(error.message || error);
   }
