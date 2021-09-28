a369544fb2d6fa5c869581c21aa37d7543d5657c
unknown <diegosc1512@gmail.com>
2021-09-28 16:21:23 -0400

    modify album component

--- itunes-search/src/app/album/album.component.spec.ts
+++ itunes-search/src/app/album/album.component.spec.ts
@@ -1,4 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 import { AlbumComponent } from './album.component';
 
@@ -6,12 +9,12 @@ describe('AlbumComponent', () => {
   let component: AlbumComponent;
   let fixture: ComponentFixture<AlbumComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
       declarations: [ AlbumComponent ]
     })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AlbumComponent);
