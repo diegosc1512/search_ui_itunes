0e47de5517a41aea0cbfc7dbb6679fcd69e54fc2
diegosc1512 <42757319+diegosc1512@users.noreply.github.com>
2021-09-29 22:08:54 -0400

    unit test artist (#1)

--- itunes-search/src/app/app.component.spec.ts
+++ itunes-search/src/app/app.component.spec.ts
@@ -1,15 +1,18 @@
 /* tslint:disable:no-unused-variable */
 
+import { Component } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+ let component : AppComponent; 
 
-describe('App: KendoAngular2QuickstartCli', () => {
+describe('App: ITUNES-SEARCH', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     });
+    component = new  AppComponent();
   });
 
   it('should create the app', async(() => {
@@ -18,16 +21,16 @@ describe('App: KendoAngular2QuickstartCli', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'Itunes search engine'`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('ITunes search engine ');
   }));
 
   it('should render title in a h1 tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('Loading...');
   }));
 });
